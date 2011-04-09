b418c8ed477fea771d8c3df8356854837466d04f
Cédric Reginster <cederigo@gmail.com>
2011-04-09 13:08:55 +0200

    search stuff

--- app/src/ch/unibe/lema/ui/BrowseActivity.java
+++ app/src/ch/unibe/lema/ui/BrowseActivity.java
@@ -4,10 +4,16 @@ import java.util.LinkedList;
 import java.util.List;
 
 import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
 import ch.unibe.lema.LemaException;
 import ch.unibe.lema.R;
 import ch.unibe.lema.provider.Filter;
@@ -17,76 +23,107 @@ import ch.unibe.lema.provider.Lecture;
 public class BrowseActivity extends BindingActivity {
     private static final String TAG_NAME = "Browse";
     private LectureListAdapter listAdapter;
-    private ProgressDialog pDialog;
+    
+    
+    private final AsyncTask<Filter, Integer, List<Lecture>> loader = 
+        new AsyncTask<Filter, Integer, List<Lecture>>() {
+
+        @Override
+        protected List<Lecture> doInBackground(Filter... filters) {
+
+            List<Lecture> result = null;
+
+            if (filters.length == 1) {
+                startWait();
+                // TODO interactive provider selection
+                try {
+                    mService.selectProvider(0);
+                    result = mService.findLectures(filters[0]);
+                } catch (LemaException e) {
+                    mService.handleError(e);
+                }
+
+                return result;
+            }
+            // return empty list
+            return new LinkedList<Lecture>();
+        }
+
+        protected void onPostExecute(final List<Lecture> result) {
+
+            listAdapter.clear();
+            for (Lecture l : result) {
+                listAdapter.add(l);
+            }
+
+            listAdapter.notifyDataSetChanged();
+            mService.showInfo("found " + result.size() + " lectures");
+            stopWait();
+        }
+
+    };
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Log.d(TAG_NAME, "onCreate");
-        setContentView(R.layout.main);
+        setContentView(R.layout.browse);
+        
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.i(TAG_NAME, "search: "+query);
+            if (mBound) {
+                loader.execute(buildFilter(query));
+            }
+        }
 
         /* setup list */
-        final ListView lectureList = (ListView) findViewById(R.id.lecturelist);
+        final ListView lectureList = (ListView) findViewById(R.id.browse_lecturelist);
         listAdapter = new LectureListAdapter(this, new LinkedList<Lecture>());
         lectureList.setAdapter(listAdapter);
+        lectureList.setOnItemClickListener(new OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                    long id) {
+                
+                Toast.makeText(getApplicationContext(), "position: "+position+" clicked. view: " + view
+                        ,5);
+                
+            }
+        });
     }
 
     public void onDestroy() {
         super.onDestroy();
     }
 
-    private Filter sampleFilter() {
-        /* TODO interactive filter creation */
+    private Filter buildFilter(String query) {
+        //simple search based on lecture title
+        //in the informatik institution at current semester
         Filter filter = new Filter();
-        FilterCriterion crit = new FilterCriterion("institution", "informatik",
-                "");
-        // FilterCriterion crit2 = new FilterCriterion("person", "strahm", "");
-        FilterCriterion crit3 = new FilterCriterion("semester", "S2011", "");
-        filter.addCriteria(crit);
-        // filter.addCriteria(crit2);
-        filter.addCriteria(crit3);
-
+        
+        filter.addCriteria(new FilterCriterion("institution", "informatik",""));
+        filter.addCriteria(new FilterCriterion("semester", "S2011", ""));
+        filter.addCriteria(new FilterCriterion("title",query,""));
+        
+        return filter;
+        
+    }
+    
+    private Filter defaultFilter() {
+        /* TODO interactive filter creation */
+        
+        Filter filter = new Filter();
+        
+        filter.addCriteria(new FilterCriterion("institution", "informatik",""));
+        filter.addCriteria(new FilterCriterion("semester", "S2011", ""));
+                
         return filter;
     }
 
     protected void serviceAvailable() {
-        AsyncTask<Filter, Integer, List<Lecture>> loader = new AsyncTask<Filter, Integer, List<Lecture>>() {
-
-            @Override
-            protected List<Lecture> doInBackground(Filter... filters) {
-
-                List<Lecture> result = null;
-
-                if (filters.length == 1) {
-                    startWait();
-                    // TODO interactive provider selection
-                    try {
-                        mService.selectProvider(0);
-                        result = mService.findLectures(filters[0]);
-                    } catch (LemaException e) {
-                        mService.handleError(e);
-                    }
-
-                    return result;
-                }
-                // return empty list
-                return new LinkedList<Lecture>();
-            }
-
-            protected void onPostExecute(final List<Lecture> result) {
-
-                listAdapter.clear();
-                for (Lecture l : result) {
-                    listAdapter.add(l);
-                }
-
-                listAdapter.notifyDataSetChanged();
-                mService.showInfo("found " + result.size() + " lectures");
-                stopWait();
-            }
-
-        };
-
-        loader.execute(sampleFilter());
+        loader.execute(defaultFilter());
     }
 }
