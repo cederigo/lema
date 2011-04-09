df3a93547f8f7c18101f344f17fc62faebbc4d01
Cédric Reginster <cederigo@gmail.com>
2011-04-09 13:09:33 +0200

    first version lecture detail screen

---
+++ app/src/ch/unibe/lema/ui/LectureActivity.java
@@ -0,0 +1,54 @@
+package ch.unibe.lema.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import ch.unibe.lema.R;
+import ch.unibe.lema.provider.Lecture;
+
+/**
+ * Display details about a Lecture
+ * @author cede
+ *
+ */
+
+public class LectureActivity extends BindingActivity {
+    
+    private static final String TAG_NAME = "Lecture";
+    private Lecture mLecture;
+
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.d(TAG_NAME, "onCreate");
+        setContentView(R.layout.lecture);
+        
+        Intent i = getIntent();
+        mLecture = i.getParcelableExtra("lecture"); 
+        if (mLecture == null) {
+            setResult(RESULT_CANCELED);
+            finish();
+            return;
+        }
+        
+        Log.i(TAG_NAME, "show "+mLecture.getNumber());
+        TextView titleView = (TextView)findViewById(R.id.lecture_title);
+        titleView.setText(mLecture.getTitle());
+        
+
+    }
+    
+    
+    
+    @Override
+    protected void serviceAvailable() {
+       /*nothing to do with the service*/
+       
+       
+       
+
+    }
+
+}
