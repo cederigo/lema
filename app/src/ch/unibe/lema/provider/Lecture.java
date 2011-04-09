f1fb2794d5e867b8d9cb8b1a3c7a422a5fadf03c
Cédric Reginster <cederigo@gmail.com>
2011-04-09 13:07:35 +0200

    implement parcelable so that we can pass lecture objects with intents

--- app/src/ch/unibe/lema/provider/Lecture.java
+++ app/src/ch/unibe/lema/provider/Lecture.java
@@ -1,11 +1,24 @@
 package ch.unibe.lema.provider;
 
-public class Lecture {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Lecture implements Parcelable{
 
     private String title;
     private String number;
     private String staff;
     private String semester;
+    
+    public Lecture() {}
+    
+    public Lecture(Parcel in) {
+        title = in.readString();
+        number = in.readString();
+        staff = in.readString();
+        semester = in.readString();
+    }
+    
 
     public void setTitle(String title) {
         this.title = title;
@@ -42,4 +55,42 @@ public class Lecture {
     public String getStaff() {
         return staff;
     }
+    
+    /**
+     * match a Lecture based on the number
+     */
+    public boolean equals(Lecture l) {
+        return l != null && l.getNumber().equalsIgnoreCase(number);
+    }
+    
+    
+    /**
+     * implement Parcelable interface so that a Lecture can easely be from Activity to Activity
+     * via Intent
+     */
+    public int describeContents() {
+        
+        return 0;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        
+        dest.writeString(title);
+        dest.writeString(number);
+        dest.writeString(staff);
+        dest.writeString(semester);
+        
+    }
+
+    public static final Parcelable.Creator<Lecture> CREATOR = new Parcelable.Creator<Lecture>() {
+        public Lecture createFromParcel(Parcel in) {
+            return new Lecture(in);
+        }
+
+        public Lecture[] newArray(int size) {
+            return new Lecture[size];
+        }
+    
+    };
+    
 }
